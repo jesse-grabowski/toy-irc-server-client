0563b77f44e7f842403a5c672c6a981f05b39a42
Jesse Grabowski <business@jessegrabowski.com>
2025-12-15 11:54:45 +0900

    Mavenify

--- run-client.sh
+++ run-client.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Find the single directory inside out/production
-PROD_DIR=$(find out/production -mindepth 1 -maxdepth 1 -type d | head -n 1)
-
-# Run the Java program using that directory as the classpath
-java -cp "$PROD_DIR" IRCClient -P abc -L FINE localhost
+java -cp target/irc-1.0.0-SNAPSHOT.jar IRCClient -P abc -L FINE localhost
